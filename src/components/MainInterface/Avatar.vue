ca8cf6540c8652a9f970d027d4f2930eb2e467ba
Daniel Oaks <daniel@danieloaks.net>
2021-08-11 21:38:47 +1000

    Add dodgy example Avatar element

---
+++ src/components/MainInterface/Avatar.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="avatar"><span>d</span></div>
+</template>
+
+<script lang="ts"></script>
+
+<style lang="scss" scoped>
+  .avatar {
+    padding: 1.05em;
+    border-radius: 1em;
+    position: relative;
+    background: #367;
+    color: #ace;
+
+    span {
+      position: absolute;
+      margin: -0.5em 0 0 -0.3em;
+    }
+  }
+</style>
--- src/components/MainInterface/MyProfilePane.vue
+++ src/components/MainInterface/MyProfilePane.vue
@@ -1,13 +1,50 @@
 <template>
-  <div class="myProfilePane"></div>
+  <div class="myProfilePane">
+    <Avatar />
+    <div class="names">
+      <span class="displayName">Daniel Oaks</span>
+      <span class="identifier">dan-</span>
+    </div>
+  </div>
 </template>
 
-<script lang="ts"></script>
+<script lang="ts">
+  import Avatar from './Avatar.vue'
+
+  export default {
+    components: {
+      Avatar,
+    },
+  }
+</script>
 
 <style lang="scss" scoped>
   .myProfilePane {
     display: flex;
     height: 3.75em;
     background: var(--sidebar-profile-bg);
+    align-items: center;
+
+    .avatar {
+      margin: 0 0.4em 0 0.5em;
+    }
+
+    .names {
+      display: flex;
+      flex-direction: column;
+      justify-items: center;
+      color: var(--sidebar-title);
+      width: 100%;
+      flex: 1 1 auto;
+
+      .displayName {
+        font-size: 1.1em;
+        margin-bottom: 0.2em;
+      }
+
+      .identifier {
+        font-size: 0.85em;
+      }
+    }
   }
 </style>
